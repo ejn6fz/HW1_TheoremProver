951b9ce350629a0a72b67fcf84edf9099c19e1ae
ejn6fz <ejn6fz@virginia.edu>
2015-02-15 20:52:00 -0500

    Parser.java

---
+++ Parser.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Parser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner (System.in);
+		
+		String input = null;
+		
+		while (true) {
+		
+			input = scan.nextLine();
+			
+			if (input.contains("->"))
+				
+			
+			
+			
+			
+			if (input.toLowerCase().equals("stop"))
+				break;
+			
+		}
+		
+	}
+	
+	public void commandParser (String input ) {
+		
+	}
+
+}
