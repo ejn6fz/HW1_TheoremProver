6be13b31d2cee60783dcf0fedeab711344e654a9
ryanmahoney <rtm8mg@virginia.edu>
2015-02-17 16:37:18 -0500

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -4,19 +4,19 @@ import java.util.LinkedList;
 public class Vertex {
 
 	String value;
-	public LinkedList <Vertex> upstream;
-	public LinkedList <Vertex> downstream;
+	public Vertex parent;
+	public LinkedList <Vertex> children;
 	
 	public Vertex(String value) {
 		this.value = value;
 	}
 	
-	public boolean addDownstream(Vertex v) {
-		return downstream.add(v);
+	public void addParent(Vertex v) {
+		parent = v;		
 	}
 	
-	public boolean addUpstream(Vertex v){
-		return upstream.add(v);
+	public boolean addChild(Vertex v){
+		return children.add(v);
 	}
 	
 }
